72d7ff088ce42d0fe8f2d14462364755bd3c5901
Алексей Главизнин <aleks@glaviznin.ru>
2017-05-08 16:27:13 +0300

    Добавьте файлы проекта.

---
+++ OutlookAddInOVA/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// сопоставленные со сборкой.
+[assembly: AssemblyTitle("OutlookAddInOVA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OutlookAddInOVA")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если необходимо обратиться к типу в этой сборке через 
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[assembly: Guid("f88d3a3b-11f7-4d6d-824c-24d4da0d5c6b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Версия
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// с помощью знака '*', как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
